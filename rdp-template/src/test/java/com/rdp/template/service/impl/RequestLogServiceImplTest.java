8b5c3f0df6a0788e562cbbfaa39b9685097c694b
Tim <303051991@qq.com>
2020-03-31 23:07:14 +0800

    +)加入测试模块

---
+++ rdp-template/src/test/java/com/rdp/template/service/impl/RequestLogServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.rdp.template.service.impl;
+
+import com.rdp.template.repository.RequestLogMapper;
+import com.rdp.template.repository.UserMasterMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestLogServiceImplTest {
+
+    @Autowired
+    private UserMasterMapper userMasterMapper;
+
+    @Test
+    public void deleteByPrimaryKey() {
+        Assert.assertEquals(100, 90);
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void insertSelective() {
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+        var name = userMasterMapper.selectByPrimaryKey("10030753").getUserName();
+        name = "";
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() {
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+    }
+
+    @Test
+    public void batchInsert() {
+    }
+}
